22694c3d0ea42c01f1abe12fdde99576273abd68
Ahmed Mohamed <78311079+ahmedmo112@users.noreply.github.com>
2022-05-17 00:26:13 +0200

    edit

--- Text Editor/TextEditorFunc.cpp
+++ Text Editor/TextEditorFunc.cpp
@@ -3,7 +3,7 @@
 // Program Description: Edit on text files
 // Last Modification Date: 14/5/2022
 // Second author - ID : Ahmed Mohamed Hany / 20210038
-// First author - ID : Salma Mohammed Mahmoud / 20210161
+// First author - ID : Ahmed Alaa / 20210038
 // Third author - ID : Ahmed Reda Elsayed / 20210018
 // Under The Supervision of: Dr. Mohamed El-Ramly
 
