1d8a06b6c817b07c4ca058417dcd52bd7568d00c
Ahmed Mohamed <78311079+ahmedmo112@users.noreply.github.com>
2022-05-17 00:20:39 +0200

    edit

--- Login Application/LoginFunctions.cpp
+++ Login Application/LoginFunctions.cpp
@@ -3,7 +3,7 @@
 // Program Description: Login System allow user to (Register - Login - Change Password)
 // Last Modification Date: 11/5/2022
 // Second author - ID : Ahmed Mohamed Hany / 20210038
-// First author - ID : Salma Mohammed Mahmoud / 20210161
+// First author - ID : Ahmed Alaa / 20210038
 // Third author - ID : Ahmed Reda Elsayed / 20210018
 // Under The Supervision of: Dr. Mohamed El-Ramly
 
